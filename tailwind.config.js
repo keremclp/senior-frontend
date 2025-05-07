796c4e68334a9f79df8a192a3a42497a66eb667d
keremclp <kerem00962@gmail.com>
2025-05-07 15:23:19 +0300

    feat: initialize project with essential configurations and dependencies
    - Added package.json with project metadata, scripts, and dependencies for Expo, React, and Tailwind CSS.
    - Created tailwind.config.js to configure Tailwind CSS with custom theme settings.
    - Added tsconfig.json for TypeScript configuration, extending Expo's base config and enabling strict type checking.

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E3A8A", // Example color
+        secondary: "#FBBF24", // Example color
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        mono: ["Fira Code", "monospace"],
+      },
+      fontSize: {
+        xs: ["0.75rem", { lineHeight: "1rem" }],
+        sm: ["0.875rem", { lineHeight: "1.25rem" }],
+        base: ["1rem", { lineHeight: "1.5rem" }],
+        lg: ["1.125rem", { lineHeight: "1.75rem" }],
+        xl: ["1.25rem", { lineHeight: "1.75rem" }],
+      },
+      spacing: {
+        0: "0px",
+        1: "4px",
+        2: "8px",
+        3: "12px",
+        4: "16px",
+        5: "20px",
+        6: "24px",
+        7: "28px",
+        8: "32px",
+        9: "36px",
+        10: "40px",
+      },
+
+    },
+  },
+  plugins: [],
+}
